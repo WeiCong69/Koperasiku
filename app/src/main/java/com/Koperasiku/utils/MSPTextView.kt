66d009f98e80d3c56a90a4d166135fa3c0fe335e
LAW WEI CONG <asarasensei@gmail.com>
2021-05-08 11:54:16 +0800

    Initial commit

---
+++ app/src/main/java/com/Koperasiku/utils/MSPTextView.kt
@@ -0,0 +1,23 @@
+package com.Koperasiku.utils
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+/**
+ * This class will be used for Custom font text using the TextView which inherits the AppCompatTextView class.
+ */
+class MSPTextView(context: Context, attrs: AttributeSet) : AppCompatTextView(context, attrs) {
+
+    init {
+        applyFont()
+    }
+
+    private fun applyFont() {
+        val typeface: Typeface =
+            Typeface.createFromAsset(context.assets, "Montserrat-Regular.ttf")
+        setTypeface(typeface)
+
+    }
+}
